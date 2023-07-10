fd69e746919ff8d9d450f1a178c5dcc8c3848208
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2023-07-10 14:05:44 +0100

    chore: add serde

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod pokemon_csv;
+use pokemon_csv::*;
+
+fn main() -> Result<(), csv::Error> {
+    let mut rdr = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
+
+    for result in rdr.deserialize() {
+        let pokemon: PokemonCsv = result?;
+        println!("{:?}", pokemon);
+    }
+
+    Ok(())
 }
